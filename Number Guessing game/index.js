1884ac7dd257f692b8c954e072c278e5fec03528
Naseem-Choudhary <naseemchoudhary149@gmail.com>
2026-01-10 13:47:58 +0530

    Third Project

---
+++ Number Guessing game/index.js
@@ -0,0 +1,23 @@
+let guess = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+let subbtn = document.getElementById("subbtn");
+let message = document.getElementById("output");
+console.log(guess);
+let attempt = 0;
+
+subbtn.onclick = function () {
+  if(attempt >= 3){
+    message.textContent = "Too many attempts"
+    subbtn.disabled = true;
+    return;
+}
+    let numinp = document.getElementById("inputnum").value;
+  attempt++;
+  if (guess == numinp) {
+    message.textContent = "Right Guess in ${attempts}";
+    subbtn.disabled = true;
+  } else if (numinp >= guess - 10 && numinp <= guess + 10) {
+    message.textContent = "Close";
+  } else {
+    message.textContent = " Too Far";
+  }
+};
